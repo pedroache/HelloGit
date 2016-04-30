bdac1ad756e0261b030d8bc5d9873dcc3283b6f5
Pedro <pedroache@gmail.com>
2016-04-30 14:57:05 -0300

    Inclusao da view no  HelloGit

--- HelloGit/src/br/com/hello/git/main/Main.java
+++ HelloGit/src/br/com/hello/git/main/Main.java
@@ -1,9 +1,11 @@
 package br.com.hello.git.main;
 
+import br.com.hello.git.view.Output;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Git!");
+		Output.helloGit();
 	}
 
 }
---
+++ HelloGit/src/br/com/hello/git/view/Output.java
@@ -0,0 +1,9 @@
+package br.com.hello.git.view;
+
+public class Output {
+	
+	public static void helloGit(){
+		System.out.println("Hello Git!");
+	}
+
+}
